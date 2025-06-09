20b1a8b773c59f1c4dac98c7b70579549100eff7
woylie <13847569+woylie@users.noreply.github.com>
2025-06-09 14:53:23 +0200

    add tag rename command

--- src/cli/tag.rs
+++ src/cli/tag.rs
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use crate::persistence::load_frames;
+use crate::persistence::{load_frames, save_frames};
 use crate::{Format, TagArgs, TagCommands};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
@@ -18,13 +18,44 @@ impl ListOutput {
     }
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RenameOutput {
+    pub old_name: String,
+    pub new_name: String,
+}
+
+impl RenameOutput {
+    fn to_text(&self) -> String {
+        format!(
+            "Tag renamed from '{}' to '{}'.",
+            self.old_name, self.new_name
+        )
+    }
+}
+
 pub fn run_tag(args: &TagArgs, config_path: &Path, format: &Format) -> anyhow::Result<()> {
-    let frames = load_frames(config_path)?;
+    let mut frames = load_frames(config_path)?;
 
     match &args.command {
         TagCommands::List => {
             let tags = frames.all_tags();
             let output = ListOutput { tags };
+
+            let output_string = match format {
+                Format::Json => serde_json::to_string_pretty(&output)?,
+                Format::Text => output.to_text(),
+            };
+
+            println!("{}", output_string);
+        }
+        TagCommands::Rename { old_name, new_name } => {
+            frames.rename_tag(old_name, new_name);
+            save_frames(config_path, &frames)?;
+
+            let output = RenameOutput {
+                old_name: old_name.to_string(),
+                new_name: new_name.to_string(),
+            };
 
             let output_string = match format {
                 Format::Json => serde_json::to_string_pretty(&output)?,
