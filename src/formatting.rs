3689292ee7589bfbe047bb865b5a5b4b4d5fe87c
woylie <13847569+woylie@users.noreply.github.com>
2025-06-17 15:34:43 +0200

    add formatting test

--- src/formatting.rs
+++ src/formatting.rs
@@ -29,8 +29,35 @@ pub fn format_duration(secs: i64) -> String {
 
     let result = parts.join(" ");
     if negative {
-        format!("-{}", result)
+        format!("-{result}")
     } else {
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_seconds_as_duration_str() {
+        let cases = [
+            (0, "0s"),
+            (1, "1s"),
+            (59, "59s"),
+            (60, "1m"),
+            (61, "1m 1s"),
+            (3599, "59m 59s"),
+            (3600, "1h"),
+            (3601, "1h 1s"),
+            (3662, "1h 1m 2s"),
+            (86400, "1d"),
+            (88888, "1d 41m 28s"),
+            (1440000, "16d 16h"),
+        ];
+
+        for (secs, expected_str) in cases {
+            assert_eq!(format_duration(secs), expected_str, "for {secs} seconds")
+        }
     }
 }
